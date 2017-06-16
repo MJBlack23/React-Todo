05b7ea8a1f161785bf7be086f30c9e71bd9de96d
Matt Black <mjblack23@Gmail.com>
2017-06-15 21:19:43 -0500

    'auto-commit'

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        TodoApp.jsx
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
