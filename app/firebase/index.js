6e51ef6bc3f5836955bb5e1c8568919f32d2effd
Matt Black <MJBlack23@Gmail.com>
2017-06-26 21:30:31 -0500

    added support for production build

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   const config = {
-      apiKey: "AIzaSyCxO6IKL8nej7qvkYPRZFyHvOZ6FCcggAY",
-      authDomain: "mjblack23-todo-app.firebaseapp.com",
-      databaseURL: "https://mjblack23-todo-app.firebaseio.com",
-      projectId: "mjblack23-todo-app",
-      storageBucket: "mjblack23-todo-app.appspot.com",
-      messagingSenderId: "137418791648"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGE_ID
     };
   firebase.initializeApp(config);
 } catch (e) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const webpack = require('webpack');
 const path = require('path');
+const envFile = require('node-env-file');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-console.log(process.env.NODE_ENV);
+console.log('Environment:',process.env.NODE_ENV);
+
+try {
+  envFile(path.join(__dirname, `'config/${process.env.NODE_ENV}.env`))
+} catch (e) {
+
+}
 
 module.exports = {
   entry: [
@@ -22,6 +29,17 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+        MESSAGE_ID: JSON.stringify(process.env.MESSAGE_ID)
       }
     })
   ],
